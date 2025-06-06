7ea7711a5a8c7bca3bbe7950a7291f6681b080cb
HarshilTandel <harshil.21beitm067@gmail.com>
2025-06-06 15:11:49 +0530

    Done Ep-3

---
+++ Chapter 03 - Let's Write the code/Code/app.js
@@ -0,0 +1,11 @@
+var name = "Harshil Tandel";
+var a = 10;
+var b = 20;
+
+console.log(name);
+console.log(a + b);
+
+// console.log(globalThis);
+
+console.log(globalThis)
+console.log(globalThis=== global)
